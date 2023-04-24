80b5f21282c509ed705c275e3423485b76c4d552
Samat <samatabzalovich@gmail.com>
2023-04-24 22:22:26 +0600

    Initial commit

---
+++ cmd/api/setup_test.go
@@ -0,0 +1,17 @@
+package main
+
+//
+//import (
+//	"greenlight.bcc/internal/data"
+//	"greenlight.bcc/internal/jsonlog"
+//	"os"
+//	"testing"
+//)
+//
+//var app application
+//
+//func TestMain(m *testing.M) {
+//	app.logger = jsonlog.New(os.Stdout, jsonlog.LevelInfo)
+//	app.models = data.NewMockModels()
+//	os.Exit(m.Run())
+//}
